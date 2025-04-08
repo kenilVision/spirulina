e26f70fb70e15953e0aea3ba940c5452f703b49c
nishan.kakadiya <nishan.kakadiya@emvigotech.com>
2025-04-08 15:02:13 +0530

    end 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { roots } from './route/Route'
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import Layout from './Layout/Layout';
+import ScrollToTop from './component/common/ScrollToTop'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         {roots.map((route, i) =><Route key={i} path={route.path} element={<Layout>{route.element}</Layout>} />)}
       </Routes>
