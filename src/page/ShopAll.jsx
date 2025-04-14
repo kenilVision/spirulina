0fe7f1edcb13999b2a6540139309bf661c47d6ce
nishan.kakadiya <nishan.kakadiya@emvigotech.com>
2025-04-14 10:29:32 +0530

    cart

--- src/page/ShopAll.jsx
+++ src/page/ShopAll.jsx
@@ -3,12 +3,12 @@ import Query from '../component/common/Query'
 import DataCard from '../component/common/DataCard'
 import { Products } from '../Constant/Product'
 function ShopAll() {
-
+  const data = Products
   return (
    <>
     <div className='w-full max-w-[1470px] mx-auto p-[15px]  pb-[50px] lg:pb-[60px] mb-0 md:mb-[50px]'>
    <Query />
-   <DataCard combinedData={Products} />
+   <DataCard combinedData={data} />
    </div>
    </>
   )
